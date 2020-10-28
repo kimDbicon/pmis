8ace1f2c7741f03da9b525cac501eef4ef9ad4b4
kimDbicon <kimryd@naver.com>
2020-10-28 14:25:38 +0900

    mni_erp push!!

---
+++ src/main/java/com/mnierp/pmis/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.mnierp.pmis.springboot.web.dto;
+
+import com.mnierp.pmis.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
